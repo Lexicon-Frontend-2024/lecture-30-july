39e374bebb9e30fff3d1a4f63ab8a60518cda92e
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2024-07-30 13:56:11 +0200

    pokemons solutions ex 1

--- script.js
+++ script.js
@@ -1,28 +1,65 @@
 console.log('Hello world!');
 
-fetchMovies();
+renderPokemons();
 
-async function fetchMovies() {
+async function renderPokemons() {
+    let pokemons = await fetchPokemons(); 
+    pokemons.forEach(pokemon => {
+        console.log(pokemon)
+        document.body.innerHTML += `${pokemon.name} <br>`;
+    });
+}
 
+async function fetchPokemons() {
     // try {
-    //     let response = await fetch('https://santosnr6.github.io/Data/movies.json');
+    //     let response = await fetch('https://santosnr6.github.io/Data/pokemons.json');
     //     let data = await response.json();
-    //     console.log(data);
-
-    //     data.forEach(movie => {
-    //         document.body.innerHTML += `${movie.title} - ${movie.imdbid} <br>`;
-    //     });
+    //     return data;
     // } catch(error) {
     //     console.log(error);
     // }
 
-    // Gamla fetch-metoden - funkar alldeles finfint!
-    fetch('https://santosnr6.github.io/Data/movies.json')
+    let pokemons = [];
+    await fetch('https://santosnr6.github.io/Data/pokemons.json')
         .then(response => response.json())
         .then(data => {
-            data.forEach(movie => {
-                document.body.innerHTML += `${movie.title} - ${movie.imdbid} <br>`;
+            data.forEach(pokemon => {
+                pokemons.push(pokemon);
             });
-        })
-        .catch(error => console.log(error));
+        });
+    return pokemons;
+}
+
+
+
+
+// Från förmiddagens genomgång
+
+// fetchMovies();
+
+async function fetchMovies() {
+
+    // Det nyare fetch-anropet
+    try {
+        let response = await fetch('https://santosnr6.github.io/Data/movies.json');
+        let data = await response.json();
+        console.log(data);
+
+        data.forEach(movie => {
+            document.body.innerHTML += `${movie.title} - ${movie.imdbid} <br>`;
+        });
+        return data;
+    } catch(error) {
+        console.log(error);
+    }
+
+    // Gamla fetch-metoden - funkar alldeles finfint!
+    // fetch('https://santosnr6.github.io/Data/movies.json')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         data.forEach(movie => {
+    //             document.body.innerHTML += `${movie.title} - ${movie.imdbid} <br>`;
+    //         });
+    //     })
+    //     .catch(error => console.log(error));
 }
